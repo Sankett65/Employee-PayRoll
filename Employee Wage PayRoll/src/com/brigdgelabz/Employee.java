80930d2373dac96493c4f45b90c1310a2259d261
Sankett65 <jadhav.jadhav.sanket@gmail.com>
2023-08-30 18:02:48 +0530

    Welcome to the Employee Wage Payroll

---
+++ Employee Wage PayRoll/src/com/brigdgelabz/Employee.java
@@ -0,0 +1,10 @@
+package com.brigdgelabz;
+
+public class Employee {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Employee Wage PayRoll");
+
+        
+
+    }
+}
