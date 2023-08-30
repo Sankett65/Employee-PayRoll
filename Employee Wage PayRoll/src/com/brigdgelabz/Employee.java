610ed957579fbf5f497d87b3549640794539ded4
Sankett65 <jadhav.jadhav.sanket@gmail.com>
2023-08-30 18:18:52 +0530

    Adding Part time

--- Employee Wage PayRoll/src/com/brigdgelabz/Employee.java
+++ Employee Wage PayRoll/src/com/brigdgelabz/Employee.java
@@ -6,13 +6,18 @@ public class Employee {
         int empCheck = 1;
         int wagePerHr=20;
         int full_time = 0;
+        int part_time=2;
         int wage;
-        double val = Math.floor(Math.random()*10 % 2);
+        double val = Math.floor(Math.random()*10) % 3;
         if (empCheck==val){
-            System.out.println("The Employee is Present");
-           full_time=8;
+            System.out.println("\nThe Employee is Present for Full day");
+           full_time=16;
+        }else if (part_time==val){
+            System.out.println("\nEmployee is Presnt for Part time");
+            full_time=8;
         }else {
-            System.out.println("The Emlployee is Absent");
+            System.out.println("\nThe Emlployee is Absent");
+
         }
         wage= full_time * wagePerHr;
         System.out.println("The Wage of the Employee is: "+wage);
