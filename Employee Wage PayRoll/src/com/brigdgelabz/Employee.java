eb925f64cb0a0eb75b2c1668340cdf1a8dd3f116
Sankett65 <jadhav.jadhav.sanket@gmail.com>
2023-08-30 18:05:53 +0530

    To check is Employee is present or absent

--- Employee Wage PayRoll/src/com/brigdgelabz/Employee.java
+++ Employee Wage PayRoll/src/com/brigdgelabz/Employee.java
@@ -3,8 +3,13 @@ package com.brigdgelabz;
 public class Employee {
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee Wage PayRoll");
-
-        
+        int empCheck = 1;
+        double val = Math.floor(Math.random()*10 % 2);
+        if (empCheck==val){
+            System.out.println("The Employee is Present");
+        }else {
+            System.out.println("The Emlployee is Absent");
+        }
 
     }
 }
