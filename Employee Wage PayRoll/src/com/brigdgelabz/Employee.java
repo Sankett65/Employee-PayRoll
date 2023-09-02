8ecb38bab1832d81eb5f03efe94fa33c8ba4ee8b
Sankett65 <jadhav.jadhav.sanket@gmail.com>
2023-09-02 15:37:19 +0530

    Employee Should Work for 100 hr in Month

--- Employee Wage PayRoll/src/com/brigdgelabz/Employee.java
+++ Employee Wage PayRoll/src/com/brigdgelabz/Employee.java
@@ -1,5 +1,7 @@
 package com.brigdgelabz;
 
+import java.util.Random;
+
 public class Employee {
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee Wage PayRoll");
@@ -9,30 +11,56 @@ public class Employee {
         int part_time=2;
         int wage=0;
         int totalWage=0;
+        int totalWorkingHr=100;
+        int emphr=0;
+     //   int totalWorkingDays=10;
+        int count =0;
+        int count1=0;
+        int count2=0;
 
-        for (int i =0;i<20;i++) {
-            double val = Math.floor(Math.random()*10) % 3;
-            switch ((int) val) {
-                case 1:
-                    full_time:
-                    System.out.println("\nEmployee is Present For Full time");
-                    full_time = 8;
-                    wage = full_time * wagePerHr;
-                    break;
-                case 2:
-                    part_time:
-                    System.out.println("\nEmployee is present for Part Time");
-                    full_time = 4;
-                    wage = full_time * wagePerHr;
-                    break;
-                default:
-                    System.out.println("\nEmployee is Abesnt");
-                    wage=0;
-                    break;
-            }
+        for (int day =1;day<=20;day++) {
+//            Random r = new Random();
+//            int n = r.nextInt(0,2);
+
+//            if (totalWorkingHr>=100 || totalWorkingDays>=20) {
+                double val = Math.floor(Math.random() * 10) % 3;
+                switch ((int) val) {
+                    case 1:
+                        full_time:
+                        System.out.println("Employee is Present For Full time for Day "+day);
+                        full_time = 7;
+                        wage = full_time * wagePerHr;
+                        count++;
+                        break;
+                    case 2:
+                        part_time:
+                        System.out.println("Employee is present for Part Time for Day "+day);
+                        full_time = 4;
+                        wage = full_time * wagePerHr;
+                        count1++;
+                        break;
+                    default:
+                        System.out.println("Employee is Abesnt for Day "+day);
+                        wage = 0;
+                        count2++;
+                        break;
+                }
+//            }
+            emphr=emphr+full_time;
+        //    System.out.println(emphr);
             totalWage=totalWage+wage;
         }
 
-        System.out.println("\nThe Wage of the Employee  for a month is: "+totalWage);
+        System.out.println("\nEmployee is Present for full time for "+count+ " day");
+        System.out.println("Employee is Present for part time for "+count1+ " day");
+        System.out.println("Employee is Absent for "+count2+ " day");
+
+        if (emphr >=100){
+           System.out.println("\nEmployee Must be Working for More than 100 hr in month" +
+                   "\nAnd Employee is work for "+emphr+" hr in month");
+           System.out.println("The Wage of the Employee  for a month is: "+totalWage);
+       }
+
+
     }
 }
